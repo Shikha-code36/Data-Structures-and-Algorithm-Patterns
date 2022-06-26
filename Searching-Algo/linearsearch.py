51867c9322be5eba14a43d07c232a9f232c30599
Wisteli-ShikhaPandey <85352745+Shikha-code36@users.noreply.github.com>
2022-06-26 21:51:30 +0530

    Linear Search

--- Searching-Algo/linearsearch.py
+++ Searching-Algo/linearsearch.py
@@ -2,12 +2,23 @@
                                 Linear Search 
 Linear search is also called as sequential search algorithm. It is the simplest searching algorithm. In Linear search, we simply traverse the list completely and match each element of the list with the item whose location is to be found. If the match is found, then the location of the item is returned; otherwise, the algorithm returns NULL.
 '''
-
-def linearsearch(n,t):
-    for i in range(len(n)):
-        if t==n[i]:
+#iterative approach
+def linearsearch(list,target):
+    for i in range(len(list)):
+        if target==list[i]:
             return i
     return -1
 
-a = [1,2,4,5,8]
-print(linearsearch(a,8)) #4
+list = [1,2,4,5,8]
+print(linearsearch(list,8)) #4
+
+#recursive approach
+def linearsearch(list,curr_index,target):
+    if curr_index == -1:
+        return -1
+    if list[curr_index] == target:
+        return curr_index
+    return linearsearch(list, curr_index-1,target)
+
+list = [1,2,4,5,8]
+print(linearsearch(list,len(list)-1,8)) #4
